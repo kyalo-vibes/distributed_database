4a893c02bc92810e4ea4b7d8c425fd80ae3c111f
Kyalo <kevin.kioko4@gmail.com>
2023-01-21 03:16:15 -0500

    Finalize siteCwindows

--- siteCwindows.py
+++ siteCwindows.py
@@ -2,9 +2,9 @@
 import mysql.connector
 import psycopg2
 
-#Site running on Windows with mysql. It stores the fragments for Derived Horizontal Fragmentation and Vertical Fragmentation
+# Site running on Windows with mysql. It performs Derived Horizontal Fragmentation
 
-# connecting to the database
+# Connecting to the MySQL database on the Windows machine
 dataBase = mysql.connector.connect(
 					host = "192.168.100.21",
                     port="3306",
@@ -12,10 +12,10 @@ dataBase = mysql.connector.connect(
 					passwd = "",
 					database = "mini_project")
 
-# preparing a cursor object
+# Preparing a cursor object for the MySQL database
 cursorObject = dataBase.cursor(buffered=True)
 
-
+# Connecting to the PostgreSQL database on the Linux machine
 local_stream = psycopg2.connect(
     host='127.0.0.1',
     port='5432',
@@ -24,12 +24,12 @@ local_stream = psycopg2.connect(
     password='postgres'
 )
 
+# Preparing a cursor object for the PostgreSQL database
 local_cursor = local_stream.cursor()
-# local_cursor.execute("USE mini_project")
 print("Derived Horizontal") 
 print("")
 # Step 1: Perform PHF on the Products table
- #Query to create fragment LOC1 WHERE [price > 200] ";
+# Query to create fragment loc1 WHERE [price > 200] ";
 cursorObject.execute("DROP TABLE IF EXISTS loc1")
 cursorObject.execute("CREATE TABLE IF NOT EXISTS loc1 (SELECT * FROM products WHERE price > 200)")
 cursorObject.execute("SELECT * FROM loc1")
@@ -38,6 +38,7 @@ print("LOC1: price > 200' ")
 print(loc1_result)
 print("")
 
+# Step 2: Perform DHF on the Inventory table. Keep dhf1 and dhf2 in same site C
 cursorObject.execute("DROP TABLE IF EXISTS dhf1")
 cursorObject.execute("CREATE TABLE IF NOT EXISTS dhf1 (SELECT inventory.warehouse_id, loc1.product_id, inventory.stock_quantity, loc1.price FROM inventory INNER JOIN loc1 ON loc1.product_id = inventory.product_id WHERE inventory.stock_quantity > 50);")
 cursorObject.execute("SELECT * FROM dhf1")
@@ -56,7 +57,7 @@ print(query_three_result)
 print("")
 #End
 
-
+# Query to create fragment loc2 WHERE [price <= 200] ";
 local_cursor.execute("DROP TABLE IF EXISTS loc2")
 local_cursor.execute("CREATE TABLE IF NOT EXISTS loc2 AS SELECT * FROM products WHERE price <= 200;")
 local_cursor.execute("SELECT * FROM loc2")
@@ -65,7 +66,7 @@ print("LOC2: price <= 200 ")
 print(loc2_result)
 print("")
 
-
+# Step 2: Perform DHF on the Inventory table. Ship dhf3 and dhf4 to PostgreSQL database
 local_cursor.execute("DROP TABLE IF EXISTS dhf3")
 local_cursor.execute("CREATE TABLE IF NOT EXISTS dhf3 AS (SELECT inventory.warehouse_id, loc2.product_id, inventory.stock_quantity, loc2.price FROM inventory INNER JOIN loc2 ON loc2.product_id = inventory.product_id WHERE inventory.stock_quantity > 50);")
 local_cursor.execute("SELECT * FROM dhf3")
